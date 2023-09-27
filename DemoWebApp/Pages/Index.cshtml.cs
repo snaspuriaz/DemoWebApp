8dba4683f1e8e6ee4529c4631bea2fdccae7be68
Shankar Naspuri <snaspuri@adventintermodal.com>
2023-09-27 23:54:40 +0530

    Add project files.

---
+++ DemoWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using DemoWebApp.Models;
+using DemoWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        //private readonly ILogger<IndexModel> _logger;
+
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public List<Product> _Prods;
+        public void OnGet()
+        {
+            ProductService prodSer = new ProductService();
+              _Prods = prodSer.GetProducts();
+
+        }
+    }
+}
---
+++ DemoWebApp/Services/ProductService.cs
@@ -0,0 +1,46 @@
+﻿using DemoWebApp.Models;
+using Microsoft.Data.SqlClient; 
+namespace DemoWebApp.Services
+{
+    public class ProductService
+    {
+        private static string db_source = "sndberver001.database.windows.net";
+        private static string db_user = "snsqladmin";
+        private static string db_password = "5naspuri@1";
+        private static string db_database = "snAppDB001";
+
+        private SqlConnection getdatabase()
+        {
+            var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_database;
+            return new SqlConnection(_builder.ConnectionString);
+
+        }
+        public List<Product> GetProducts()
+        {
+            SqlConnection conn = getdatabase();
+            List<Product> lstProducts = new List<Product>();
+            string sqlStatment = "select ProductID,ProductName,Quantity from Products";
+            conn.Open();
+            SqlCommand sqlcmd = new SqlCommand(sqlStatment, conn);
+            using(SqlDataReader sqlReader = sqlcmd.ExecuteReader())
+            {
+                while(sqlReader.Read())
+                {
+                    Product prod = new Product()
+                    {
+                        ProductId = sqlReader.GetInt32(0),
+                        ProductName = sqlReader.GetString(1),
+                        Quantity = sqlReader.GetInt32(2)
+                    };
+                    lstProducts.Add(prod);
+                }
+            }
+            conn.Close();
+            return lstProducts;
+        }
+    }
+}
